33d8cdaed22535db55b68484357e865ea1efd91c
Potatoasdf <113385901+Potatoasdf@users.noreply.github.com>
2023-01-12 11:26:11 -0800

    asdasdasdasdasd
    asdasdasdasdasd

--- Node.h
+++ Node.h
@@ -1,1 +1,19 @@
 
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+#include "Student.h"
+using namespace std;
+
+class Node {
+ public:
+  Node(Student* newstudent);
+  ~Node();
+  Node* getNext();
+  Student* getStudent();
+  void setNext(Node* newnext);
+  Node* next; 
+  Student* student;
+
+};
+#endif
